a56862b375be2608432f892cb867c5d03ac89fe0
Stefan0333 <49637187+Stefan0333@users.noreply.github.com>
2022-03-24 22:15:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 var express = require('express');
-// const { getMaxListeners } = require('process');
+var mysql = require('mysql2');
+var ejs = require('ejs');
+var https = require('https');
+const { getMaxListeners } = require('process');
+const { rootCertificates } = require('tls');
 var app = express();
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'St3f@n!!',
+    database: 'frizeraj'
+});
+
+connection.connect();
 app.use(express.static('views'));
 app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
-    res.render('index');
-});
+//app.get('/', (req, res) => {
+  //  res.render('index');
+//});
 
-// app.get('/', (req, res) => {
 
-//     connection.query('SELECT * from grad', function (err, rows, fields) {
-//         if (err) throw err;
-//         res.render('index', { REZULTAT: rows });
-//     });
-    
-// });
+ app.get('/', (req, res) => {
+
+     connection.query('SELECT * from klijent', function (err, result) {
+         if (err) throw err;
+         res.render('index', { REZULTAT: result });
+         if(err) throw err;
+         console.log(result);
+     });
+
+ });
 
 app.listen(3000);
