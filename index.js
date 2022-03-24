4ed01a6608e69bd1dbc5604b7555a0c0a3628b5b
Stefan <85559438+StefanVidacak03@users.noreply.github.com>
2022-03-24 21:30:35 +0100

    Commit 3
    Svi .html fajlovi prebaceni u .ejs

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ var express = require('express');
 // const { getMaxListeners } = require('process');
 var app = express();
 
-app.use(express.static('public'));
-// app.set('view engine', 'ejs');
-// connection.connect();
+app.use(express.static('views'));
+app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + "/public/index.html");
+    res.render('index');
 });
 
 // app.get('/', (req, res) => {
